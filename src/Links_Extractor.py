ecf4ae96746b75cc94049d4c85831ee51378ac0d
Hussain <78584556+hussain5416@users.noreply.github.com>
2021-12-27 21:36:25 +0530

    Refactoring (#34)
    Code refactored a lot

--- src/Links_Extractor.py
+++ src/Links_Extractor.py
@@ -18,291 +18,168 @@ from colorama import Fore
 
 
 
-def take_user_input(question_text:str, replace_tuple_list=[]):
-    """
-    This function asks user input and returns the modified text
-    """    
-    
-    x = input(
-        f'{Fore.WHITE}> {question_text}: {Fore.LIGHTBLUE_EX}'
-    )
-    
-    if replace_tuple_list != []:
-        for rep_tuple in replace_tuple_list:
-            x.replace(
-                rep_tuple[0],
-                rep_tuple[1]
-            )
-
-    return x
-
-
+# Choices dict
+choiceDict :dict[str, tuple[str, str]] = {
+    '1': ('Web links', '(http/https)'),
+    '2': ('FTP links', '(ftp)'),
+    '3': ('MAIL links', '(mailto)'),
+    '4': ('All types of links', ''),
+    '5': ('Web-Crawl', '(all links)'),
+    '6': ('About', '')
+}
+choicesText = ''
+for a, b in choiceDict.items():
+    choicesText += f' {a} - {b[0]} {b[1]}\n'
 
 
 
 class Extract_Links:
-    """
-    Main Links Extraction class
-    """    
 
     def __init__(self):
         super().__init__()
-        self.webcrawl = False
-        
         try:
             # User choices
-            choices_dict, self.user_choice = self.asks_user_choice()
+            print(Fore.WHITE, end='')
+            self.userChoice = self.getUserChoice()
             print(f'{Fore.RESET}', end='')
             
-            if self.user_choice == '6':
-                self.show_about_data()
-                raise ZeroDivisionError
-            
-            # Local file Crawling
-            ques = 'Enter file name to extract links from it (relative/absolute path)'
-            rep_list = [('/', '\\')]
-            
-            # Web Crawling
-            if self.user_choice == '5':
-                ques = 'Enter WEB-Page address to extract links from it (Ex: github.com/hussain5416)'
-                rep_list = [('\\', '/')]
-                self.webcrawl = True
-                
-            # Old location user input
-            self.source_location = take_user_input(
-                question_text=ques,
-                replace_tuple_list=rep_list
-            )
-            if self.webcrawl:
-                if '://' not in self.source_location:
-                    self.source_location = f'https://{self.source_location}'        # Add https://, if not present
-            
-            # NEW FILE - for extracted links
-            self.new_file_location = self.get_extracted_file_location(choices_dict)
-                
-            ## Main links extraction function
-            self.main_extracting_fctn()
+            # Main Action
+            match self.userChoice:
+                case '6':   self.show_about_data()
+                case _:     self.mainExtraction()
         
-        except ZeroDivisionError:                                   # [False Error] For user initiated errors
-            pass
-            
+        except FileNotFoundError:
+            print(f'{Fore.RED}=> [Error] Source not found. Write proper file name...')
+                
         except Exception as e:
-            print(
-                f'{Fore.RED}=> [Error 1] {e}, Try again...'
-            )
+            print(f'{Fore.RED}=> [Error] {e}, Try again...')
     
-    
-    def get_extracted_file_location(self, choices_dict):
-        """
-        - RETURNS the file location of NEW FILE, for extracted links
-        - Creates parent folder - if missing
-        """        
-        
-        # New File Name
-        file_name = self.source_location
-        for i in ['\\', '/', ':', '*', '?', '"', '<', '>', '|']:
-            file_name = file_name.replace(i, '-')
-        new_f_name = f'{choices_dict[self.user_choice][0]} - {file_name}.txt'
-        
-        # Parent Folder location
-        new_folder_location = os.path.join(
-            winshell.desktop(),
-            'Extracted Links'
-        )
-        if not os.path.exists(new_folder_location):
-            os.makedirs(new_folder_location)
-            
-        # New File location
-        new_file_loctn = os.path.join(
-            new_folder_location,
-            new_f_name
-        )
-        
-        return new_file_loctn
-        
-    
-    def asks_user_choice(self):
+    def getUserChoice(self):
         """
         This function shows user the choices and returns dict and the user choice
-        """        
-        
-        # Choices dict
-        choice_dict = {
-            '1': ('Web links', '(http/https)'),
-            '2': ('FTP links', '(ftp)'),
-            '3': ('MAIL links', '(mailto)'),
-            '4': ('All types of links', ''),
-            '5': ('Web-Crawl', '(all links)'),
-            '6': ('About', '')
-        }
-        
+        """
         # Printing choices
-        print(Fore.WHITE, end='')
         print(app_name.center(os.get_terminal_size().columns))
-        for key, val in choice_dict.items():
-            print('', key, '-', val[0], val[1])
+        print(choicesText)
         
         # Asking for user choice
-        choice_question = f'Enter your choice ({"/".join(choice_dict.keys())})'
-        choice = take_user_input(choice_question)
-        while not choice in choice_dict.keys():
-            choice = take_user_input(choice_question)
+        question = f'Enter your choice ({"/".join(choiceDict)})'
+        choice = self.takeUserInput(question)
+        while choice not in choiceDict:
+            choice = self.takeUserInput(question)
         print()
         
-        return choice_dict, choice
+        return choice
     
-
-    def main_extracting_fctn(self):
+    def mainExtraction(self):
+        """ Main Links extraction """
+        # Ask for source location -> Get its data
+        match self.userChoice:
+            case '5':                                                                                       # Web
+                self.sourcePath = self.takeUserInput('Enter web-address (Ex: github.com/hussain5416)', [('\\', '/')], web=True)
+                sourceData = requests.get(self.sourcePath).text
+            case _:                                                                                         # Local-file
+                self.sourcePath = self.takeUserInput('Enter relative/absolute file-path', [('/', '\\')])
+                try:
+                    with open(self.sourcePath, encoding='utf-8') as f:  sourceData = f.read()
+                except Exception:
+                    with open(self.sourcePath) as f:                    sourceData = f.read()
         
-        try:
-            ## Getting data from source location
-            if self.webcrawl:
-                self.data_to_parse = requests.get(
-                    self.source_location
-                ).text                                                              # Source code of webpage
-                self.user_choice = '4'                                                      # for extracting all links
-            else:                                                                   # Data from file
-                try:                                                                        # solve encoding issues
-                    with open(self.source_location, encoding='utf-8') as f:
-                        self.data_to_parse = f.read()
-                except:
-                    with open(self.source_location) as f:
-                        self.data_to_parse = f.read()
-                        
-            self.extract_links_from_string()                    # Extract links
-            self.write_data_to_file()                           # Write links to a file
-            
-        except FileNotFoundError:
-            print(
-                f'{Fore.RED}=> [Error] "{self.source_location}" not found. Write the proper file name...'
-            )
-                
-        except Exception as e:
-            print(
-                f'{Fore.RED}=> [Error 2] {e}, Try again...'
-            )
-            
-        else:
-            # Open saved extracted links file
-            General_Class.open_file(
-                file_to_open=self.new_file_location
-            )
+        # Extract links
+        retData = self.get_extractedLinks(sourceData)
+        
+        # Save data -> open saved file
+        fileLocation = self.get_filePath()
+        self.saveToFile(fileLocation, retData[0], retData[1])
+        threading.Thread(target=lambda: os.system(f'""{fileLocation}""'), daemon=True).start()
 
 
-    def extract_links_from_string(self):
+    ## -------------------------------------------- Others -------------------------------------------- ##
+    @staticmethod
+    def takeUserInput(question:str, replaceList=[], web:bool=False):
+        """ Asks user input & Returns: modified text """    
+        # Input
+        x = input(f'{Fore.WHITE}> {question}: {Fore.LIGHTBLUE_EX}')
+        
+        # Modifications
+        if replaceList:
+            for i in replaceList:
+                x = x.replace(i[0], i[1])
+        if web and '://' not in x:
+            x = 'https://' + x
+
+        return x
+
+    def get_filePath(self):
+        """ Returns: File location where extracted links would be saved """
+        # File Name
+        fileName = self.sourcePath
+        for i in ['\\', '/', ':', '*', '?', '"', '<', '>', '|']:
+            fileName = fileName.replace(i, '-')
+        fileName = f'{choiceDict[self.userChoice][0]} - {fileName}.txt'
+        
+        # Parent directory
+        dirPath = os.path.join(winshell.desktop(), 'Extracted Links')
+        if not os.path.exists(dirPath):
+            os.makedirs(dirPath)
+            
+        # File path
+        return os.path.join(dirPath, fileName)
+    
+    def get_extractedLinks(self, sourceData:str):
         """
-        This function extracts links from source location based on user choice
-        """            
+        Returns: (`extracted-links`, `extraction-summary`)
+        """
+        def getLinks(regex:str) -> set[str] :
+            return set(re.findall(regex, sourceData, re.IGNORECASE))
         
         # Extracting links from file
-        def extract_links_proper(regex_string:str):
-            """
-            This function returns a 'LIST' of matching items, based on the passed 'REG-EX'
-            """
-            
-            extracted_list = General_Class.non_duplicated_list(
-                re.findall(
-                    regex_string,
-                    self.data_to_parse,
-                    re.IGNORECASE
-                )
-            )
+        webLinks   = getLinks(r'(https?://[^("\s<>)]+)')               # http[s]://anything_until (' ', <, >)
+        ftpLinks   = getLinks(r'(ftp://[^("\s<>)]+)')                  # ftp://anything_until (' ', <, >)
+        mailLinks  = getLinks(r'(mailto: *[^("\s<>)]+)')               # mailto:[whitespaces]anything_until (' ', <, >)
 
-            return extracted_list
+        # Extracted links
+        if self.userChoice == '5': self.userChoice = '4'
+        match self.userChoice:
+            case '1':   text, linksList = '◆ Web links:', webLinks
+            case '2':   text, linksList = '◆ FTP links:', ftpLinks
+            case '3':   text, linksList = '◆ Mail links:', mailLinks
+            case '4':   text, linksList = '◆ All links:', webLinks.union(ftpLinks, mailLinks)
+            case _  :   raise ValueError('Wrong inputs')
         
-        web_links = extract_links_proper(
-            r'(https?://[^("\s<>)]+)'                    # http[s]://anything_until (' ', <, >)
-        )
-        ftp_list = extract_links_proper(
-            r'(ftp://[^("\s<>)]+)'                       # ftp://anything_until (' ', <, >)
-        )
-        mail_links = extract_links_proper(
-            r'(mailto: *[^("\s<>)]+)',                   # mailto:[whitespaces]anything_until (' ', <, >)
-        )
+        # Extraction summary
+        summaryDict = {text: len(linksList)}
+        if self.userChoice == '4':
+            summaryDict.update({'        • Web links:'  : len(webLinks)})
+            summaryDict.update({'        • FTP links:'  : len(ftpLinks)})
+            summaryDict.update({'        • Mail links:' : len(mailLinks)})
+        summaryDict.update({'◆ Words:': len(sourceData.split(' '))})
+        summaryDict.update({'◆ Lines:': len(sourceData.split('\n'))})
+        return linksList, summaryDict
 
-        # User choice :: Links & Additional data
-        user_choice_dict = {
-            '1': (
-                '◆ Web links:',
-                web_links
-            ),
-            
-            '2': (
-                '◆ FTP links:',
-                ftp_list
-            ),
-            
-            '3': (
-                '◆ Mail links:',
-                mail_links
-            ),
-            
-            '4': (
-                '◆ All Links:',
-                web_links + ftp_list + mail_links
-            ),
-        }
-        
-        # Links to extract
-        self.extracted_items_list = user_choice_dict[self.user_choice][1]
-        
-        # Additional data
-        self.additional_items_dict = {}
-        self.additional_items_dict.update(
-            {
-                user_choice_dict[self.user_choice][0]: len(user_choice_dict[self.user_choice][1])
-            }
-        )
-        if self.user_choice == '4':
-            self.additional_items_dict.update(
-                {
-                    '        • Web links:': len(web_links),
-                    '        • FTP links:': len(ftp_list),
-                    '        • Mail links:': len(mail_links)
-                }
-            )
-        self.additional_items_dict.update(
-            {
-                '◆ Words:': len(self.data_to_parse.split(' ')),
-                '◆ Lines:': len(self.data_to_parse.split('\n'))
-            }
-        )
-
-
-    def write_data_to_file(self):
+    def saveToFile(self, fileLocation, extractedLinks:set[str], summary:dict[str, int]):
         """
         Function to write extracted links to a new file
         """
-        
-        with open(self.new_file_location, 'a+', encoding='utf-8') as f_new:
-            
-            # Heading
-            f_new.writelines("•" * 84)
-            f_new.writelines(
-                f'\n● Links extracted from "{self.source_location}"\n'
-                f'● {General_Class.get_current_date_and_time()}\n\n'
-            )
-            
-            # Conclusion
-            conclusion = '\n'.join(
-                [f'{key} {val}' for key, val in self.additional_items_dict.items()]
-            )
-            f_new.writelines(f'{conclusion}\n\n')
+        with open(fileLocation, 'a+', encoding='utf-8') as f:
+            # Summary
+            currentTime = datetime.now().strftime(r'%d/%b/%Y   %I:%M %p')
+            summaryData = [f'{a} {b}' for a, b in summary.items()]
+            summaryStr = '\n'.join(summaryData + ['\n'])
+            f.write("•" * 84)
+            f.write(f'\n● Links extracted from "{self.sourcePath}"\n● {currentTime}\n\n')
+            f.write(summaryStr)
             
             # Links
-            for num, link in enumerate(self.extracted_items_list, start=1):
-                print(f'{Fore.GREEN}{num} -- Extracted -- {link}')
-                f_new.writelines(f'    {num} - {link}\n')
-
-            f_new.writelines("‾" * 100)
-            f_new.writelines('\n\n\n')
+            for i, link in enumerate(extractedLinks, start=1):
+                print(f'{Fore.GREEN}{i} -- Extracted -- {link}')
+                f.write(f'    {i} - {link}\n')
+            f.writelines(['‾'*100, '\n\n\n'])
             
-        # Print conclusion
         print(
-            f'{Fore.BLUE}{conclusion}\n'
-            f'{Fore.YELLOW}=> Data saved to "{self.new_file_location}"'
-        )
-
+            f'{Fore.BLUE}{summaryStr}'
+            f'{Fore.YELLOW}=> Data saved to "{fileLocation}"'
+        )   # Summary
 
     def show_about_data(self):
         print(
@@ -311,7 +188,7 @@ class Extract_Links:
             "Creator: Hussain Abbas\n" \
             f"App Webpage: {github_link}\n"
         )
-        update_check = take_user_input('Check for updates? (y/n) ')
+        update_check = self.takeUserInput('Check for updates? (y/n) ')
         print(f'{Fore.GREEN}', end='')
         if update_check.lower() in ['y', 'yes']:
             threading.Thread(
@@ -326,65 +203,6 @@ class Extract_Links:
 
 
 
-
-class General_Class:
-    """
-    This class contains general methods
-    """        
-
-    @staticmethod
-    def get_current_date_and_time():
-        """
-        Returns: string: customised date and time
-        """
-        # Current Date
-        months_list = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-        day_today = datetime.now().day
-        if day_today < 10:
-            day_today = f'0{day_today}'
-        current_date = f'{day_today}/{months_list[datetime.now().month - 1]}/{datetime.now().year}'
-        
-        # Current Time (12hr format)
-        hour = datetime.now().hour
-        minute = datetime.now().minute
-        time_stamp = 'am'                   # AM
-        if hour >= 12:                      # PM
-            time_stamp = 'pm'
-            if hour != 12:
-                hour = hour - 12
-        if minute < 10:
-            minute = f'0{minute}'
-        current_time = f'{hour}:{minute} {time_stamp}'
-        
-        return f'{current_date}   {current_time}'
-
-
-    @staticmethod
-    def open_file(file_to_open):
-        """
-        This function launches a file using subprocess module
-            - Threading is used to keep the app functional
-        """
-        
-        file_thread = threading.Thread(
-            target=lambda : os.system(f'""{file_to_open}""'),
-        )
-        file_thread.setDaemon(True)
-        file_thread.start()
-    
-    
-    @staticmethod
-    def non_duplicated_list(original_list):
-        """
-        This function returns a list after removing all the duplicates
-        """
-
-        new_list = []
-        [new_list.append(item) for item in original_list if item not in new_list]
-        
-        return new_list
-
-    
 
 
 ############################################################################################## Run Main Program
