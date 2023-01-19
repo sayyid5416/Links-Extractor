fba22469cda53983e9a185a6b1aa9d195940e537
Sayyid <78584556+sayyid5416@users.noreply.github.com>
2023-01-19 11:14:32 +0530

    Formatting

--- src/main.py
+++ src/main.py
@@ -24,31 +24,50 @@ import requests
 
 ## ----------------------------------------------- Prints ----------------------------------------------- ##
 def pp(text:str, fore=Fore.WHITE, end:str='\n'):
-    return print(f'{fore}{text}{Fore.WHITE}', end=end)
+    return print(
+        f'{fore}{text}{Fore.WHITE}',
+        end=end
+    )
 
 def pp_info(text):
-    return pp(text, Fore.GREEN)
+    return pp(
+        text,
+        Fore.GREEN
+    )
 
 def pp_info_2(text):
-    return pp(text, Fore.CYAN)
+    return pp(
+        text,
+        Fore.CYAN
+    )
 
 def pp_error(text):
-    return pp(text, Fore.LIGHTRED_EX)
+    return pp(
+        text,
+        Fore.LIGHTRED_EX
+    )
 
 def pp_input(text):
-    return pp(text, Fore.LIGHTBLUE_EX)
+    return pp(
+        text,
+        Fore.LIGHTBLUE_EX
+    )
 
 def pp_question(text):
-    return input(f'{Fore.WHITE}> {text}: {Fore.LIGHTBLUE_EX}')
+    return input(
+        f'{Fore.WHITE}> {text}: {Fore.LIGHTBLUE_EX}'
+    )
     
 
 
 ## ----------------------------------------------- Directory ----------------------------------------------- ##
-
 def get_desktop_path():
     """ Returns: Desktop path, create if not exist """
     desktop = os.path.join(
-        os.environ.get('USERPROFILE', '/'), 
+        os.environ.get(
+            'USERPROFILE',
+            '/'
+        ), 
         'Desktop'
     )
     if not os.path.exists(desktop):
@@ -57,7 +76,10 @@ def get_desktop_path():
 
 def get_saving_directory():
     """ Returns: Path of directory where extracted links will be saved , create if not exist """
-    dirPath =  os.path.join(get_desktop_path(), 'Extracted Links')
+    dirPath =  os.path.join(
+        get_desktop_path(),
+        'Extracted Links'
+    )
     if not os.path.exists(dirPath):
         os.makedirs(dirPath)
     return dirPath
@@ -65,11 +87,17 @@ def get_saving_directory():
 
 # Settings folder
 _settingsDir = os.path.join(
-    os.environ.get('LOCALAPPDATA', 'App-Data'),
+    os.environ.get(
+        'LOCALAPPDATA',
+        'App-Data'
+    ),
     'Links-Extractor'
 )
 # Settings file
-_settingsFile = os.path.join(_settingsDir, 'links-extractor.json')
+_settingsFile = os.path.join(
+    _settingsDir,
+    'links-extractor.json'
+)
 
 
 
@@ -90,7 +118,12 @@ def get_settings_file():
     # Write default settings file -> If missing
     if not os.path.exists(_settingsFile):
         with open(_settingsFile, 'w') as f:
-            json.dump(_default_settings, f, indent=4, sort_keys=True)
+            json.dump(
+                _default_settings,
+                f,
+                indent=4,
+                sort_keys=True
+            )
     
     return _settingsFile
 
@@ -98,7 +131,9 @@ def get_settings_file():
 def get_current_settings() -> dict[str, bool] :
     """ Returns: all current settings """
     with open(get_settings_file(), 'r') as f:
-        return json.load(f)
+        return json.load(
+            f
+        )
 
 
 def get_specific_setting(setting:str):
@@ -106,18 +141,30 @@ def get_specific_setting(setting:str):
     settings = get_current_settings()
     return settings.get(
         setting,
-        _default_settings.get(setting, False)
+        _default_settings.get(
+            setting, 
+            False
+        )
     )
 
 
 def set_settings(setting:str, value:bool):
     """ Sets a new setting """
-    settings = get_current_settings()               # current settings
-    settings.update({setting: value})               # updated settings
-
+    # Settings
+    settings = get_current_settings()                                                       # current settings
+    settings.update(
+        {
+            setting: value
+        }
+    )                                                                                       # updated settings
     # Save new settings
     with open(get_settings_file(), 'w') as f:
-        json.dump(settings, f, indent=4, sort_keys=True)
+        json.dump(
+            settings,
+            f,
+            indent=4,
+            sort_keys=True
+        )
     
 
 ## ------------- Settings
@@ -125,8 +172,13 @@ def switch_raw_setting():
     """ Enable / Disable raw settings """
     oldRawSetting = get_specific_setting('raw')
     newRawSetting = not oldRawSetting
-    set_settings('raw', newRawSetting)
-    pp_info(f'[Raw formatting {"ENABLED" if newRawSetting else "DISABLED"}]')
+    set_settings(
+        'raw',
+        newRawSetting
+    )
+    pp_info(
+        f'[Raw formatting {"ENABLED" if newRawSetting else "DISABLED"}]'
+    )
 
 
 
@@ -136,7 +188,10 @@ def D_error_catcher(func:Callable):
     """ Decorator to catch errors """
     def wrapper(*args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            return func(
+                *args,
+                **kwargs
+            )
         except KeyboardInterrupt:
             pp_input(f'<ctrl+c>')
             pp_info('[Exited]\n\n')
@@ -157,13 +212,34 @@ def get_choices(asString:bool=False):
     
     # Choices
     _choices :dict[str, tuple[str, str]] = {
-        '1': ('[File] Web links', '(http/https)'),
-        '2': ('[File] FTP links', '(ftp)'),
-        '3': ('[File] MAIL links', '(mailto)'),
-        '4': ('[File] All types of links', ''),
-        'web': ('[Web-Crawl] All types of links', ''),
-        'about': ('About', ''),
-        'raw': ('Enable/Disable raw formatting of links', f'({rawSetting})')
+        '1': (
+            '[File] Web links', 
+            '(http/https)'
+        ),
+        '2': (
+            '[File] FTP links',
+            '(ftp)'
+        ),
+        '3': (
+            '[File] MAIL links',
+            '(mailto)'
+        ),
+        '4': (
+            '[File] All types of links',
+            ''
+        ),
+        'web': (
+            '[Web] All types of links', 
+            ''
+        ),
+        'about': (
+            'About',
+            ''
+        ),
+        'raw': (
+            'Enable/Disable raw formatting of links', 
+            f'({rawSetting})'
+        )
     }
     
     # If as string:
@@ -178,7 +254,9 @@ def get_choices(asString:bool=False):
 
 _choiceDict = get_choices()
 if isinstance(_choiceDict, str):
-    raise TypeError(f'"choiceDict" should be a "dict", and not a "str"')
+    raise TypeError(
+        f'"choiceDict" should be a "dict", and not a "str"'
+    )
 choiceDict = _choiceDict
 
 
@@ -188,24 +266,35 @@ class Extract_Links:
     @D_error_catcher
     def __init__(self):
         # User choices
-        pp(app_name.center(os.get_terminal_size().columns), end='\n\n')
+        pp(
+            app_name.center(
+                os.get_terminal_size().columns
+            ),
+            end='\n\n'
+        )
         self.userChoice = self.getUserChoice()
         
         # Main Action
         match self.userChoice:
             case 'about':   self.show_about_data()
-            case 'raw': switch_raw_setting()
-            case _:     self.mainExtraction()
+            case 'raw':     switch_raw_setting()
+            case _:         self.mainExtraction()
     
     
     def getUserChoice(self):
         """ Show user the choices and returns dict and the user choice """
         # Printing choices
-        print(get_choices(asString=True))
+        print(
+            get_choices(
+                asString=True
+            )
+        )
         
         # Asking for user choice
         while True:
-            choice = self.takeUserInput(f'Enter your choice ({"/".join(choiceDict)})')
+            choice = self.takeUserInput(
+                f'Enter your choice ({"/".join(choiceDict)})'
+            )
             if choice in choiceDict:
                 print()
                 return choice
@@ -215,27 +304,56 @@ class Extract_Links:
         """ Main Links extraction """
         # Ask for source location -> Get its data
         match self.userChoice:
-            case 'web':                                                                                       # Web
-                self.sourcePath = self.takeUserInput('Enter web-address (Ex: github.com/sayyid5416)', [('\\', '/')], web=True)
-                sourceData = requests.get(self.sourcePath).text
-            case _:                                                                                         # Local-file
-                self.sourcePath = self.takeUserInput('Enter relative/absolute file-path', [('/', '\\')])
+            case 'web':                                                                                         # Web
+                self.sourcePath = self.takeUserInput(
+                    'Enter web-address (Ex: github.com/sayyid5416)', 
+                    [
+                        (
+                            '\\',
+                            '/'
+                        )
+                    ],
+                    web=True
+                )
+                sourceData = requests.get(
+                    self.sourcePath
+                ).text
+            case _:                                                                                             # Local-file
+                self.sourcePath = self.takeUserInput(
+                    'Enter relative/absolute file-path',
+                    [
+                        (
+                            '/',
+                            '\\'
+                        )
+                    ]
+                )
                 try:
-                    with open(self.sourcePath, encoding='utf-8') as f:  sourceData = f.read()
+                    with open(self.sourcePath, encoding='utf-8') as f:
+                        sourceData = f.read()
                 except Exception:
-                    with open(self.sourcePath) as f:                    sourceData = f.read()
+                    with open(self.sourcePath) as f:
+                        sourceData = f.read()
         
         # Extract links
         retData = self.get_extractedLinks(sourceData)
         
         # Save data -> open saved file
-        self.saveToFile(retData[0], retData[1])
-        threading.Thread(target=lambda: os.system(f'""{self.get_filePath()}""'), daemon=True).start()
+        self.saveToFile(
+            retData[0],
+            retData[1]
+        )
+        threading.Thread(
+            target=lambda: os.system(
+                f'""{self.get_filePath()}""'
+            ),
+            daemon=True
+        ).start()
 
 
     ## -------------------------------------------- Others -------------------------------------------- ##
     @staticmethod
-    def takeUserInput(question:str, replaceList=[], web:bool=False):
+    def takeUserInput(question: str, replaceList: list[tuple[str, str]]=[], web: bool=False):
         """ Asks user input & Returns: modified text """    
         # Input
         x = pp_question(question)
@@ -243,7 +361,10 @@ class Extract_Links:
         # Modifications
         if replaceList:
             for i in replaceList:
-                x = x.replace(i[0], i[1])
+                x = x.replace(
+                    i[0],
+                    i[1]
+                )
         if web and '://' not in x:
             x = 'https://' + x
 
@@ -253,23 +374,41 @@ class Extract_Links:
         """ Returns: File location where extracted links would be saved """
         # File Name
         fileName = self.sourcePath
-        for i in ['\\', '/', ':', '*', '?', '"', '<', '>', '|']:
-            fileName = fileName.replace(i, '-')
+        for i in [
+            '\\',
+            '/',
+            ':',
+            '*',
+            '?',
+            '"',
+            '<',
+            '>',
+            '|'
+        ]:
+            fileName = fileName.replace(
+                i, 
+                '-'
+            )
         fileName = f'{choiceDict[self.userChoice][0]} - {fileName}.txt'
         
         # File path
-        filePath = os.path.join(get_saving_directory(), fileName)
-
-        return filePath
+        return os.path.join(
+            get_saving_directory(),
+            fileName
+        )
     
     def get_extractedLinks(self, sourceData:str):
-        """
-        Returns: (`extracted-links`, `extraction-summary`)
-        """
-        _userChoice = str(self.userChoice)
+        """ Returns: `(extracted-links, extraction-summary)` """
+        _userChoice = self.userChoice
         
         def getLinks(regex:str) -> set[str] :
-            return set(re.findall(regex, sourceData, re.IGNORECASE))
+            return set(
+                re.findall(
+                    regex,
+                    sourceData,
+                    re.IGNORECASE
+                )
+            )
         
         # Extracting links from file
         webLinks   = getLinks(r'(https?://[^("\s<>)]+)')               # http[s]://anything_until (' ', <, >)
@@ -277,22 +416,66 @@ class Extract_Links:
         mailLinks  = getLinks(r'(mailto: *[^("\s<>)]+)')               # mailto:[whitespaces]anything_until (' ', <, >)
 
         # Extracted links
-        if _userChoice == 'web': _userChoice = '4'
         match _userChoice:
-            case '1':   text, linksList = '◆ Web links:', webLinks
-            case '2':   text, linksList = '◆ FTP links:', ftpLinks
-            case '3':   text, linksList = '◆ Mail links:', mailLinks
-            case '4':   text, linksList = '◆ All links:', webLinks.union(ftpLinks, mailLinks)
-            case _  :   raise ValueError('Wrong inputs')
+            case '1':
+                text = '◆ Web links:'
+                linksList = webLinks
+            case '2':
+                text = '◆ FTP links:'
+                linksList = ftpLinks
+            case '3':
+                text = '◆ Mail links:'
+                linksList = mailLinks
+            case '4' | 'web':
+                text = '◆ All links:'
+                linksList = webLinks.union(
+                    ftpLinks,
+                    mailLinks
+                )
+            case _  :   
+                raise ValueError(
+                    'Wrong inputs'
+                )
         
         # Extraction summary
-        summaryDict = {text: len(linksList)}
-        if _userChoice == '4':
-            summaryDict.update({'        • Web links:'  : len(webLinks)})
-            summaryDict.update({'        • FTP links:'  : len(ftpLinks)})
-            summaryDict.update({'        • Mail links:' : len(mailLinks)})
-        summaryDict.update({'◆ Words:': len(sourceData.split(' '))})
-        summaryDict.update({'◆ Lines:': len(sourceData.split('\n'))})
+        summaryDict = {
+            text: len(linksList)
+        }
+        if _userChoice in ['4', 'web']:
+            summaryDict.update(
+                {
+                    '        • Web links:'  : len(webLinks)
+                }
+            )
+            summaryDict.update(
+                {
+                    '        • FTP links:'  : len(ftpLinks)
+                }
+            )
+            summaryDict.update(
+                {
+                    '        • Mail links:' : len(mailLinks)
+                }
+            )
+        summaryDict.update(
+            {
+                '◆ Words:': len(
+                    sourceData.split(
+                        ' '
+                    )
+                )
+            }
+        )
+        summaryDict.update(
+            {
+                '◆ Lines:': len(
+                    sourceData.split(
+                        '\n'
+                    )
+                )
+            }
+        )
+        
         return linksList, summaryDict
 
     def saveToFile(self, extractedLinks:set[str], summary:dict[str, int]):
@@ -303,9 +486,17 @@ class Extract_Links:
         rawEnabled = get_specific_setting('raw')
         with open(fileLocation, 'a+', encoding='utf-8') as f:
             # Summary
-            currentTime = datetime.now().strftime(r'%d/%b/%Y   %I:%M %p')
-            summaryData = [f'{a} {b}' for a, b in summary.items()]
-            summaryStr = '\n'.join(summaryData + ['\n'])
+            currentTime = datetime.now().strftime(
+                r'%d/%b/%Y   %I:%M %p'
+            )
+            summaryData = [
+                f'{a} {b}' for a, b in summary.items()
+            ]
+            summaryStr = '\n'.join(
+                summaryData + [
+                    '\n'
+                ]
+            )
             f.write("•" * 84)
             if rawEnabled: 
                 f.write("\n----- RAW FORMAT -----")
@@ -315,11 +506,20 @@ class Extract_Links:
             # Links
             for i, link in enumerate(extractedLinks, start=1):
                 threading.Thread(
-                    target=lambda: pp_info(f'{i} -- Extracted -- {link}')
+                    target=lambda: pp_info(
+                        f'{i} -- Extracted -- {link}'
+                    )
                 ).start()
-                if rawEnabled:  f.write(f'{link}\n')
-                else:           f.write(f'    {i} - {link}\n')
-            f.writelines(['‾'*100, '\n\n\n'])
+                if rawEnabled:
+                    f.write(f'{link}\n')
+                else:
+                    f.write(f'    {i} - {link}\n')
+            f.writelines(
+                [
+                    '‾' * 100, 
+                    '\n\n\n'
+                ]
+            )
         
         # Summary
         pp_info_2(summaryStr)
@@ -330,7 +530,9 @@ class Extract_Links:
             'app name': 'Links Extractor',
             'saving directory': get_saving_directory(),
             'settings file': get_settings_file(),
-            'creator': os.path.split(github_link)[0],
+            'creator': os.path.split(
+                github_link
+            )[0],
             'app webpage': github_link,
         }
         statement = ''
@@ -339,8 +541,13 @@ class Extract_Links:
         print(statement)
         pp_info_2('Press <ctrl+c> any time to exit the app\n')
 
-        update_check = self.takeUserInput('Check for updates? (y/n) ')
-        if update_check.lower() in ['y', 'yes']:
+        update_check = self.takeUserInput(
+            'Check for updates? (y/n) '
+        )
+        if update_check.lower() in [
+            'y', 
+            'yes'
+        ]:
             threading.Thread(
                 target=lambda: webbrowser.open(
                     f'{github_link}/releases/latest'
